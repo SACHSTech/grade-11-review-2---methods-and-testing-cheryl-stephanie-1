3d50281e33608a30f78cf9d78e0a38550310da91
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-09-27 10:06:16 -0400

    Files

--- src/gr11review/part2/FileIO2.java
+++ src/gr11review/part2/FileIO2.java
@@ -1,5 +1,8 @@
 package gr11review.part2;
 
 public class FileIO2 {
-    
+    public static void main(String[] args) throws Exception{
+        
+
+    }
 }
--- src/gr11review/part2/FileIO3.java
+++ src/gr11review/part2/FileIO3.java
@@ -1,5 +1,8 @@
 package gr11review.part2;
 
 public class FileIO3 {
-    
+    public static void main(String[] args) throws Exception{
+        
+
+    }
 }
